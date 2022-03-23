d502a6025bf922f66877a856f9156f3f5fd2ffe0
lenguyengiabao <lenguyengiabao46@gmail.com>
2022-03-24 01:08:17 +0700

    run test face embbeding

--- test_face_verify.py
+++ test_face_verify.py
@@ -1,24 +1,41 @@
-from turtle import back
 import insightface
 from insightface.app import FaceAnalysis
 import cv2 
 import numpy as np
 import glob 
 import os 
+from utils import *
 
 model_name = 'buffalo_m'
 app = FaceAnalysis(name=model_name, allowed_modules=['detection', 'recognition']) # enable detection model only
 app.prepare(ctx_id=0, det_size=(640, 640))
 
-dataset_path = '../data_verify/KomNET_dataset/Social Media/resize 224x224_sosmed/train/*/*.*'
-for path in glob.glob(dataset_path):
-    # extension = path.split('.')[-1]
-    # if extension == "jpg" or extension == "png" or extension == "jpeg":
-    background = np.zeros((500,500,3))
-    print(path)
-    img = cv2.imread(path)
-    print(img.shape)
-    background[100:324, 100:324, :] = img
-    cv2.imwrite('test.jpg', background)
-    print(app.get(background))
-    exit()
+# app = insightface.model_zoo.get_model('/home/giabao/.insightface/models/buffalo_l/w600k_r50.onnx')
+# app.prepare(ctx_id=0)
+
+dataset_path = '../data_verify/KomNET_dataset/*/'
+# dataset_path = '../data_verify/KomNET_dataset/Social Media/original_training_sosmed/train/*/*.*'
+
+# print(len(glob.glob(dataset_path)))
+sim_list = []
+for folder in glob.glob(dataset_path):
+    paths = glob.glob(folder + '*.*')[:2]
+    path1 = paths[0]
+    path2 = paths[1]
+
+    img1 = cv2.imread(path1)
+    img2 = cv2.imread(path2)
+
+    emb1 = app.get(img1)[0].embedding
+    emb2 = app.get(img2)[0].embedding
+    if emb1.shape == emb2.shape:
+        sim = findCosineDistance(emb1, emb2)
+        print(sim)
+
+        sim_list.append(sim)
+    
+print(len(sim_list))
+with open('sim_same_person.txt', 'w') as f:
+    for sim in sim_list:
+        f.write(str(sim)+'\n')
+    # exit()
