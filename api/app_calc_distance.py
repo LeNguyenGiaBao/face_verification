de7d22304c4632cd498af8f5b1006a0f3999cbcb
lenguyengiabao <lenguyengiabao46@gmail.com>
2022-05-23 22:32:54 +0700

    modify calc distance api, test api and README

--- api/app_calc_distance.py
+++ api/app_calc_distance.py
@@ -20,25 +20,34 @@ def index():
 
 @app.post("/distance/")
 async def distance(in_emb_list: List[str] = Form(...), out_emb_list: List[str] = Form(...)):
-    if ENCODE_TYPE == "string": 
-        if FACE_EMB_MODEL == "opencv":
-            in_emb = np.array([np.fromstring(i, sep=',') for i in in_emb_list])
-            out_emb = np.array([np.fromstring(i, sep=',') for i in out_emb_list])
+    try:
+        if ENCODE_TYPE == "string": 
+            if FACE_EMB_MODEL == "opencv":
+                in_emb = np.array([np.fromstring(i, sep=',') for i in in_emb_list])
+                out_emb = np.array([np.fromstring(i, sep=',') for i in out_emb_list])
 
-            if len(in_emb_list) == 1:
-                in_emb = in_emb.reshape(-1, 128)
-            if len(out_emb_list) == 1:
-                out_emb = out_emb.reshape(-1, 128)
-    if MEASURE == 'euclidean': 
-        distance = findEuclideanDistance(in_emb, out_emb)
+                if len(in_emb_list) == 1:
+                    in_emb = in_emb.reshape(-1, 128)
+                if len(out_emb_list) == 1:
+                    out_emb = out_emb.reshape(-1, 128)
+        if MEASURE == 'euclidean': 
+            distance = findEuclideanDistance(in_emb, out_emb)
 
-    return jsonable_encoder({
-        "code": 200,
-        "distance": str(distance),
-        "measure": MEASURE,
-        "model": FACE_EMB_MODEL,
-        "encode_type": ENCODE_TYPE
-    })
+        return jsonable_encoder({
+            "code": 200,
+            "distance": str(distance),
+            "measure": MEASURE,
+            "model": FACE_EMB_MODEL,
+            "encode_type": ENCODE_TYPE
+        })
+
+    except Exception as e:
+        print(e)
+        return jsonable_encoder({
+                "code": 201,
+                "error_code": 0,
+                "msg": str(e)
+            })
 
 if __name__ == "__main__":
     # run API
